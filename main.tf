167c23c2634feb3e0b42028e6251da7128c9b8e1
Iliana Chakarova <42681006+ilianachakarova@users.noreply.github.com>
2024-08-09 22:24:36 +0200

    init repo

---
+++ main.tf
@@ -0,0 +1,92 @@
+# 1. Specify the version of the AzureRM Provider to use
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.113.0"
+    }
+  }
+}
+#Configure  the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = "${var.resource_group_name}-${random_integer.ri.result}"
+  location = var.resource_group_location
+}
+
+#Create Linux App service plan
+resource "azurerm_service_plan" "asp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = "${var.resource_group_name}-${random_integer.ri.result}"
+  location            = var.resource_group_location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+
+resource "azurerm_mssql_server" "ams" {
+  name                         = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name          = "${var.resource_group_name}-${random_integer.ri.result}"
+  location                     = var.resource_group_location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+
+}
+
+resource "azurerm_mssql_database" "amssqldb" {
+  name           = "${var.sql_database_name}-${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.ams.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  sku_name       = "S0"
+  zone_redundant = false
+  max_size_gb    = 2
+}
+
+resource "azurerm_mssql_firewall_rule" "mssql_firewall" {
+  name             = "${var.firewall_rule_name}-${random_integer.ri.result}"
+  server_id        = azurerm_mssql_server.ams.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = "${var.resource_group_name}-${random_integer.ri.result}"
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.ams.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.amssqldb.name};User ID=${azurerm_mssql_server.ams.administrator_login};Password=${azurerm_mssql_server.ams.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+
+resource "azurerm_app_service_source_control" "apssc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.repo_URL
+  branch                 = "main"
+  use_manual_integration = false
+}
